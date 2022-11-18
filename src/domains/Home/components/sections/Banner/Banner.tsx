dbd79b65dbbbb7e7a87f8723adab95410340b249
schatzen <gomieti@naver.com>
2022-11-18 15:01:07 +0900

    feat: banner 동영상 추가

--- src/domains/Home/components/sections/Banner/Banner.tsx
+++ src/domains/Home/components/sections/Banner/Banner.tsx
@@ -1,3 +1,42 @@
+import { css } from "@emotion/react";
+import Image from "next/image";
+
 export const Banner = () => {
-  return <div></div>;
+  return (
+    <div css={sx.root}>
+      <div css={sx.videoFrame}>
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          preload="metadata"
+          css={sx.video}
+          src={"/assets/video/banner-video.mp4"}
+        />
+      </div>
+    </div>
+  );
+};
+
+const sx = {
+  root: css`
+    width: 100%;
+  `,
+  videoFrame: css`
+    width: 100%;
+    position: relative;
+    z-index: 0;
+    aspect-ratio: 1/0.562;
+    overflow: hidden;
+    background-color: black;
+  `,
+  video: css`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    object-fit: cover;
+    z-index: 1;
+  `,
 };

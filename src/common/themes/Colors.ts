aa050ccf180ba59bcb59f3cbb93fc8b402d3929e
schatzen <gomieti@naver.com>
2022-11-18 17:25:28 +0900

    feat: 컬러 추가

--- src/common/themes/Colors.ts
+++ src/common/themes/Colors.ts
@@ -3,15 +3,15 @@ import { PaletteOptions } from "@mui/material";
 export enum Color {
   Primary = "#ffffff",
   Secondary = "#f2f2f2",
+
+  LightWhite = "#F1F1F1",
+  LightPink = "#FF71E4",
+  LightGreen = "#A1FF92",
+  LightBlue = "#4ADEFF",
 }
 
 export const lightPalette: PaletteOptions = {
   primary: {
     main: Color.Primary,
   },
-  secondary: {
-    main: Color.Secondary,
-  },
-  text: {},
-  background: {},
 };

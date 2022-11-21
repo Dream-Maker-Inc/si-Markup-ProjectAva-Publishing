b51ff18021864a2a881e7915b76315f834321e0e
schatzen <gomieti@naver.com>
2022-11-21 15:03:41 +0900

    update: line 섹션 구현

--- src/domains/Home/components/sections/Line/Line.tsx
+++ src/domains/Home/components/sections/Line/Line.tsx
@@ -1,3 +1,41 @@
+import { css } from "@emotion/react";
+import { Typography } from "@mui/material";
+
 export const Line = () => {
-  return <div></div>;
+  const text = "Virtual Celebrity Living in the Metaverse";
+
+  let list = [];
+
+  for (let i = 0; i < text.length; i++) {
+    list.push(text.charAt(i));
+  }
+  return (
+    <div css={sx.root}>
+      {list.map((it, index) => (
+        <Typography key={index} fontFamily={"Porter Sans Block"} css={sx.text}>
+          {it === " " ? "\u00A0" : it}
+        </Typography>
+      ))}
+    </div>
+  );
+};
+
+const sx = {
+  root: css`
+    width: 100%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 8px;
+  `,
+  text: css`
+    font-size: 2.3vw;
+    line-height: 56px;
+  `,
+  span: css`
+    width: 100%;
+    height: 1em;
+    display: inline-block;
+  `,
 };

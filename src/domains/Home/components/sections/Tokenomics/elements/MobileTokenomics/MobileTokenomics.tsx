74699f9cc77c780fbf2620b2d01d482cfc4b2b7f
schatzen <gomieti@naver.com>
2022-11-25 14:56:28 +0900

    Update MobileTokenomics.tsx

--- src/domains/Home/components/sections/Tokenomics/elements/MobileTokenomics/MobileTokenomics.tsx
+++ src/domains/Home/components/sections/Tokenomics/elements/MobileTokenomics/MobileTokenomics.tsx
@@ -18,14 +18,11 @@ export const MobileTokenomics = () => {
 const sx = {
   root: css`
     width: 100%;
-    // mobile height : 1300px
-    //aspect-ratio: 1/3.61;
     background-color: black;
     padding: 0 5.55vw;
     padding-top: 22.22vw;
     padding-bottom: 11.11vw;
   `,
-
   container: css`
     width: 100%;
     height: 100%;

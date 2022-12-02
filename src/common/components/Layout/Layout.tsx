101939dd586adce53bdb486f63978c09e886f6dc
schatzen <gomieti@naver.com>
2022-12-02 14:11:37 +0900

    feat: 메타 태그 정보 추가

--- src/common/components/Layout/Layout.tsx
+++ src/common/components/Layout/Layout.tsx
@@ -11,9 +11,39 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <Stack width="100%" height="100%">
       <Head>
+        <title>Project Ailey</title>
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" href="favicon.ico" />
+
+        <meta name="author" content="Golden Egg Foundation" />
+        <meta
+          name="description"
+          content="Virtual Celebrity Living in the Metaverse World"
+        />
+        <meta
+          name="keywords"
+          content="virtual human, virtual influencer, digital human, digital influencer, robot, AI, deep learning, metaverse, game, video game, meta world, metabus, innovation, virtual reality, community"
+        />
+        <meta property="og:title" content="Project Ailey" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="assets/thumbnail/thumbnailpng" />
+        <meta property="og:article:author" content="Golden Egg Foundation" />
+        <meta
+          property="og:description"
+          content="Project Ailey is a virtual human project, a celebrity who lives and moves in a virtual space and will be active in spaces such as games, movies, and metaverse in the future."
+        />
+        <meta property="og:site_name" content="Project Ailey" />
+        <meta property="og:locale" content="en_US" />
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="google-site-verification"
+          content="ANI4_rOJc728yDE0naHGRWAsEturpmT0dfAtw5uK61U"
         />
       </Head>
       <Stack direction="row" width="100%" height="100%">

d958a4e411be7108974176ec65ae45294bd26045
schatzen <gomieti@naver.com>
2022-12-05 14:25:47 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    loader: "akamai",
+    path: "./",
     unoptimized: true,
   },
+  assetPrefix: ".",
 };
 
 module.exports = nextConfig;
